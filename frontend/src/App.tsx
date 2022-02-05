f7c31042132125583a9645afd36579e09d843b10
etotasam <spanish2007@gmail.com>
2022-02-05 16:28:25 +0900

    create login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import axios, { isAxiosError } from "./libs/axios";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
+import { Login } from "./pages/login";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/pages/login/login.tsx
@@ -0,0 +1,43 @@
+import axios, { isAxiosError } from "@/libs/axios";
+import { useState, useEffect } from "react";
+import Button from "../../components/Button";
+
+export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const submit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.get("sanctum/csrf-cookie");
+      const { data } = await axios.post("/api/login");
+      console.log(data);
+    } catch (error) {
+      if (isAxiosError(error)) {
+        console.log(error.response);
+      }
+    }
+  };
+  return (
+    <>
+      <h1>login</h1>
+      <form onSubmit={submit}>
+        <input
+          className="border border-gray-500 rounded px-1"
+          type="text"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+          placeholder="Email"
+        />
+        <input
+          className="border border-gray-500 rounded px-1"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          placeholder="Password"
+        />
+        <Button>ログイン</Button>
+      </form>
+    </>
+  );
+};
