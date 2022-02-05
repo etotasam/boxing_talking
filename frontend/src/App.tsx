7cb58017a9b0780cc72f899e39a1b6cbbe011ea3
etotasam <spanish2007@gmail.com>
2022-02-06 08:04:56 +0900

    test

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
-import { Login } from "./pages/loginform";
+import { Login } from "./pages/login";
 
 function App() {
   return (
