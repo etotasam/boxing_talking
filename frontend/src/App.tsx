73f24c6fde790e2376a364ae735fc7c49438d8c8
etotasam <spanish2007@gmail.com>
2022-05-25 17:13:52 +0900

    修正

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Home } from "@/pages/Home/Home";
+import { Home } from "@/pages/Home";
 import { Check } from "@/pages/check";
 import { Match } from "@/pages/Match";
 import { MatchRegister } from "@/pages/MatchRegister";
---
+++ frontend/src/pages/Home/Home.test.not.tsx
@@ -0,0 +1,80 @@
+import { Home } from ".";
+import { render, screen } from "@testing-library/react";
+// import { useUser, useHasAuth } from "@/store/slice/authUserSlice";
+import { useAuth } from "@/libs/hooks/useAuth";
+// import { useMatches } from "@/store/slice/matchesSlice";
+import { useFetchMatches } from "@/libs/hooks/useMatches";
+
+jest.mock("@/libs/hooks/useAuth");
+const useAuthMock = useAuth as jest.Mock;
+// const useHasAuthMock = useHasAuth as jest.Mock;
+
+jest.mock("@/libs/hooks/useMatches");
+const useFetchMatchesMock = useFetchMatches as jest.Mock;
+
+jest.mock("react-redux");
+jest.mock("react-router-dom", () => ({
+  Link: () => {
+    return (
+      <a data-testid="login-button" href="/login">
+        Loginページ
+      </a>
+    );
+  },
+  useNavigate: () => {
+    return jest.fn();
+  },
+}));
+
+describe("ログインしてない場合", () => {
+  afterAll(() => {
+    jest.resetAllMocks();
+  });
+  beforeEach(() => {
+    useAuthMock.mockReturnValue({ name: "" });
+    // useMathcesMock.mockReturnValue([{ id: 1, date: "1999/1/1" }]);
+    useFetchMatchesMock.mockReturnValue({
+      matches: [{ id: 1, date: "1999/1/1" }],
+    });
+    // useHasAuthMock.mockReturnValue(AuthIs.FALSE); //ログインの有無
+  });
+  afterEach(() => {});
+  // useSelectorMock.mockReturnValue(false)
+  it("名前がゲストとなっているかどうか", () => {
+    render(<Home />);
+    expect(screen.getByTestId(`guest`)).toBeTruthy();
+    expect(screen.getByText(/ゲストさん/)).toBeTruthy();
+  });
+
+  it("loginボタンはある、logoutボタンはない", () => {
+    render(<Home />);
+    const loginButton = screen.getByTestId("login-button");
+    const logoutButton = screen.queryByTestId("logout-button");
+    expect(loginButton).toBeTruthy();
+    expect(logoutButton).toBeNull();
+  });
+});
+
+describe("ログインしてる時", () => {
+  beforeEach(() => {
+    useAuthMock.mockReturnValue({ name: "てすと" });
+    // useMathcesMock.mockReturnValue([{ id: 1, date: "1999/1/1" }]);
+    useFetchMatchesMock.mockReturnValue({
+      matches: [{ id: 1, date: "1999/1/1" }],
+    });
+    // useHasAuthMock.mockReturnValue(AuthIs.TRUE); //ログインの有無
+  });
+  afterEach(() => {});
+  it("名前がログインユーザ名になっている", () => {
+    render(<Home />);
+    expect(screen.getByTestId(`name`)).toBeTruthy();
+    expect(screen.getByText(/てすとさん/)).toBeTruthy();
+  });
+  it("loginボタンはない、logoutボタンはある", () => {
+    render(<Home />);
+    const loginButton = screen.queryByTestId("login-button");
+    const logoutButton = screen.getByTestId("logout-button");
+    expect(loginButton).toBeNull();
+    expect(logoutButton).toBeTruthy();
+  });
+});
