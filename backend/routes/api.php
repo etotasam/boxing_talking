ae3b4d13346f528500223f97319e5e27564e7491
etotasam <spanish2007@gmail.com>
2023-09-22 20:46:00 +0900

    fixed views/emails/register.blade.php

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -134,11 +134,11 @@ Route::put("/{id}/test", function ($id = null) {
 Route::get('/test', function () {
 
     try {
-        $secret_key = config('const.jwt_secret_key');
-        if (!isset($secret_key)) {
-            throw new Exception("cannot get secret-key", 500);
-        }
-        return "fetch data complete";
+        $url = config('app.url');
+        // if (!isset($secret_key)) {
+        //     throw new Exception("cannot get secret-key", 500);
+        // }
+        return $url;
     } catch (Exception $e) {
         return response()->json(["message" => $e->getMessage()], 500);
     }
