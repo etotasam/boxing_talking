d5cd51c87b11ee7029618995d61ce318de0c69d2
etotasam <spanish2007@gmail.com>
2024-02-28 23:37:58 +0900

    Feature: backend: create middleware
    convert HTTP request camel case to snake case

---
+++ backend/app/Http/Middleware/ApiRequestMiddleware.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class ApiRequestMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $requestData = $request->all();
+        $snakeCaseData = $this->recursiveSnakeCase($requestData);
+        $request->replace($snakeCaseData);
+        \Log::debug($request);
+
+        return $next($request);
+    }
+
+    /**
+     * keyをスネークケースに変換
+     * 
+     * @param array
+     * @return 
+     */
+    private function recursiveSnakeCase(array $data): array
+    {
+        return collect($data)->map(function ($value, $key) {
+            $snakeKey = \Str::snake($key);
+            return [$snakeKey => is_array($value) ? $this->recursiveSnakeCase($value) : $value];
+        })->collapse()->all();
+    }
+}
