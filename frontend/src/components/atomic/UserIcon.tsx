d7f6042808ed75e516c1a5accf44470c6dea8cf5
masato <aret.asam40@gmail.com>
2026-07-01 18:36:15 +0900

    refactor(frontend): 未使用コンポーネントを削除

--- frontend/src/components/atomic/UserIcon.tsx
+++
@@ -1,9 +0,0 @@
-import { AiOutlineUser } from 'react-icons/ai';
-
-export const UserIcon = () => {
-  return (
-    <span>
-      <AiOutlineUser className="mr-1 block bg-cyan-700 text-white mt-[2px] w-[16px] h-[16px] rounded-[50%]" />
-    </span>
-  );
-};
